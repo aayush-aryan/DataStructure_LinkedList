5005c3c3f5d11037eec98e7a722d0b62fc7ccd03
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-30 00:28:27 +0530

    creating a linkedList

---
+++ DataStructureLinkedList/LinkedLists.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureLinkedList
+{
+    class LinkedLists
+    {
+        public Node Head;
+        public Node Tail;
+        
+        public LinkedLists()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked List:");
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write(" -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -7,6 +7,16 @@ namespace DataStructureLinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To LinkedList Section1 ! ");
+            LinkedLists linkedLists = new LinkedLists();
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
+            node1.next = node2;
+            node2.next = node3;
+            linkedLists.Head = node1;
+            linkedLists.Display();
+
+
         }
     }
 }
